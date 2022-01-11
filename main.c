73e870155734d55ddd86a80f7d791d13776bfec1
Catia Filiberto <cfiliber@e4r8p4.42roma.it>
2022-01-11 20:01:38 +0100

    11_01_2

--- main.c
+++ main.c
@@ -6,11 +6,33 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 19:23:44 by cfiliber          #+#    #+#             */
-/*   Updated: 2022/01/11 18:20:26 by cfiliber         ###   ########.fr       */
+/*   Updated: 2022/01/11 20:00:23 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	destroy_mutexes()
+{
+	
+}
+
+int	end(t_data *data, t_philo *philos_arr)
+{
+	int	i;
+	
+	if (pthread_join(data->id_death_thread, NULL) != 0)
+		return (error("death pthread_join failed"));
+	i = 0;
+	while (i < data->philos_nb)
+	{
+		if (pthread_join(philos_arr[i].thread_id, NULL) != 0)
+			return (error_thread("pthread_join failed", philos_arr[i].id));
+		i++;
+	}
+	//chiama mutex_destroy per ogni mutex
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
--- threads_2.c
+++ threads_2.c
@@ -6,30 +6,8 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 19:36:29 by cfiliber          #+#    #+#             */
-/*   Updated: 2022/01/11 19:37:52 by cfiliber         ###   ########.fr       */
+/*   Updated: 2022/01/11 20:00:17 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-int	destroy_mutexes()
-{
-	
-}
-
-int	end(t_data *data, t_philo *philos_arr)
-{
-	int	i;
-	
-	if (pthread_join(data->id_death_thread, NULL) != 0)
-		return (error("death pthread_join failed"));
-	i = 0;
-	while (i < data->philos_nb)
-	{
-		if (pthread_join(philos_arr[i].thread_id, NULL) != 0)
-			return (error_thread("pthread_join failed", philos_arr[i].id));
-		i++;
-	}
-	//chiama mutex_destroy per ogni mutex
-	return (1);
-}
